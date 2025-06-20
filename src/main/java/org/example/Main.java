ce737002f3ef8da5593a46cedafeddec36a2270c
Ishai Marom <maromish@gmail.com>
2025-06-20 19:35:23 +0300

    return to the past

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        ManualPerson manualPerson = new ManualPerson();
+        manualPerson.setNames(List.of(null));
     }
 }
