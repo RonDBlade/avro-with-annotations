4889f79c4482ba1e0f935991b014c1efb75847d7
Ishai Marom <maromish@gmail.com>
2025-06-27 20:00:01 +0300

    format

--- src/main/java/org/example/AvroClassProcessor.java
+++ src/main/java/org/example/AvroClassProcessor.java
@@ -24,7 +24,6 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.Optional;
 
 public class AvroClassProcessor {
     private static final String NOT_NULL_ANNOTATION = "org.jetbrains.annotations.NotNull";
