67347d1b56f2902cb220c30cb42184907f5c49a5
Ishai Marom <maromish@gmail.com>
2025-08-29 20:02:02 +0300

    trying

--- src/test/kotlin/UsageTest.kt
+++ src/test/kotlin/UsageTest.kt
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test
 class UsageTest {
 
     @Test
-    fun `test 1`() {
-        val k = MapsObject.newBuilder()
+    fun `map test`() {
+        val mapsObject = MapsObject.newBuilder()
             .setMapWithRegularValues(mutableMapOf("1" to 1))
             .setMapWithNullableValues(mutableMapOf<String, Int?>("2" to null))
             .setMapWithRegularMapWithRegularValues(mapOf("1" to mapOf("10" to 10)))
@@ -21,22 +21,37 @@ class UsageTest {
             .setNullableMapWithNullableMapWithNullableValues(mapOf("1" to null))
             .build()
 
-        val a1: Map<String, Int> = k.mapWithRegularValues
-        val a2: Map<String, Int?> = k.mapWithNullableValues
-        val a3: Map<String, Map<String, Int>> = k.mapWithRegularMapWithRegularValues
-        val a4: Map<String, Map<String, Int?>> = k.mapWithRegularMapWithNullableValues
-        val a5: Map<String, Map<String, Int>?> = k.mapWithNullableMapWithRegularValues
-        val a6: Map<String, Map<String, Int?>?> = k.mapWithNullableMapWithNullableValues
-        val a7: Map<String, Int>? = k.nullableMapWithRegularValues
-        val a8: Map<String, Int?>? = k.nullableMapWithNullableValues
-        val a9: Map<String, Map<String, Int>>? = k.nullableMapWithRegularMapWithRegularValues
-        val a10: Map<String, Map<String, Int?>>? = k.nullableMapWithRegularMapWithNullableValues
-        val a11: Map<String, Map<String, Int>?>? = k.nullableMapWithNullableMapWithRegularValues
-        val a12: Map<String, Map<String, Int?>?>? = k.nullableMapWithNullableMapWithNullableValues
+        val mapVariable1: Map<String, Int> = mapsObject.mapWithRegularValues
+        val mapVariable2: Map<String, Int?> = mapsObject.mapWithNullableValues
+        val mapVariable3: Map<String, Map<String, Int>> = mapsObject.mapWithRegularMapWithRegularValues
+        val mapVariable4: Map<String, Map<String, Int?>> = mapsObject.mapWithRegularMapWithNullableValues
+        val mapVariable5: Map<String, Map<String, Int>?> = mapsObject.mapWithNullableMapWithRegularValues
+        val mapVariable6: Map<String, Map<String, Int?>?> = mapsObject.mapWithNullableMapWithNullableValues
+        val mapVariable7: Map<String, Int>? = mapsObject.nullableMapWithRegularValues
+        val mapVariable8: Map<String, Int?>? = mapsObject.nullableMapWithNullableValues
+        val mapVariable9: Map<String, Map<String, Int>>? = mapsObject.nullableMapWithRegularMapWithRegularValues
+        val mapVariable10: Map<String, Map<String, Int?>>? = mapsObject.nullableMapWithRegularMapWithNullableValues
+        val mapVariable11: Map<String, Map<String, Int>?>? = mapsObject.nullableMapWithNullableMapWithRegularValues
+        val mapVariable12: Map<String, Map<String, Int?>?>? = mapsObject.nullableMapWithNullableMapWithNullableValues
 
-        k.mapWithRegularValues.remove("1")
+        mapsObject.mapWithRegularValues = mapOf("1" to 1) as Map<String, Int>
+        mapsObject.nullableMapWithNullableValues = null as Map<String, Int>?
+        mapsObject.mapWithNullableValues = mapOf("1" to null) as Map<String, Int?>
+        mapsObject.nullableMapWithNullableValues = null as Map<String, Int?>?
+        mapsObject.mapWithRegularMapWithRegularValues = mapOf("1" to mapOf("1" to 1)) as Map<String, Map<String, Int>>
+        mapsObject.mapWithRegularMapWithNullableValues = mapOf("1" to mapOf("1" to null)) as Map<String, Map<String, Int?>>
+        mapsObject.mapWithNullableMapWithRegularValues = mapOf("1" to null) as Map<String, Map<String, Int>?>
+        mapsObject.mapWithNullableMapWithNullableValues = mapOf("1" to null) as Map<String, Map<String, Int?>?>
+        mapsObject.nullableMapWithRegularMapWithRegularValues = null as Map<String, Map<String, Int>>?
+        mapsObject.nullableMapWithRegularMapWithNullableValues = null as Map<String, Map<String, Int?>>?
+        mapsObject.nullableMapWithNullableMapWithRegularValues = null as Map<String, Map<String, Int>?>?
+        mapsObject.nullableMapWithNullableMapWithNullableValues = null as Map<String, Map<String, Int?>?>?
+    }
 
-        val l = ListsObject.newBuilder()
+    @Test
+    fun `list test`() {
+
+        val listsObject = ListsObject.newBuilder()
             .setListWithRegularValues(listOf(1))
             .setNullableListWithRegularValues(listOf(2))
             .setListWithNullableValues(listOf(10))
@@ -51,51 +66,31 @@ class UsageTest {
             .setNullableListWithNullableListWithNullableValues(listOf(listOf(60)))
             .build()
 
-        val ll1: MutableList<Int> = l.listWithRegularValues
-        val ll2: MutableList<Int>? = l.nullableListWithRegularValues
-        val ll3: MutableList<Int?> = l.listWithNullableValues
-        val ll4: MutableList<Int?>? = l.nullableListWithNullableValues
-        val ll5: MutableList<MutableList<Int>> = l.listWithRegularListWithRegularValues
-        val ll6: MutableList<MutableList<Int?>> = l.listWithRegularListWithNullableValues
-        val ll7: MutableList<MutableList<Int>?> = l.listWithNullableListWithRegularValues
-        val ll8: MutableList<MutableList<Int?>?> = l.listWithNullableListWithNullableValues
-        val ll9: MutableList<MutableList<Int>>? = l.nullableListWithRegularListWithRegularValues
-        val ll10: MutableList<MutableList<Int?>>? = l.nullableListWithRegularListWithNullableValues
-        val ll11: MutableList<MutableList<Int>?>? = l.nullableListWithNullableListWithRegularValues
-        val ll12: MutableList<MutableList<Int?>?>? = l.nullableListWithNullableListWithNullableValues
+        val listVariable1: MutableList<Int> = listsObject.listWithRegularValues
+        val listVariable2: MutableList<Int>? = listsObject.nullableListWithRegularValues
+        val listVariable3: MutableList<Int?> = listsObject.listWithNullableValues
+        val listVariable4: MutableList<Int?>? = listsObject.nullableListWithNullableValues
+        val listVariable5: MutableList<MutableList<Int>> = listsObject.listWithRegularListWithRegularValues
+        val listVariable6: MutableList<MutableList<Int?>> = listsObject.listWithRegularListWithNullableValues
+        val listVariable7: MutableList<MutableList<Int>?> = listsObject.listWithNullableListWithRegularValues
+        val listVariable8: MutableList<MutableList<Int?>?> = listsObject.listWithNullableListWithNullableValues
+        val listVariable9: MutableList<MutableList<Int>>? = listsObject.nullableListWithRegularListWithRegularValues
+        val listVariable10: MutableList<MutableList<Int?>>? = listsObject.nullableListWithRegularListWithNullableValues
+        val listVariable11: MutableList<MutableList<Int>?>? = listsObject.nullableListWithNullableListWithRegularValues
+        val listVariable12: MutableList<MutableList<Int?>?>? = listsObject.nullableListWithNullableListWithNullableValues
 
-        l.listWithRegularValues = listOf(1) as List<Int>
-        l.nullableListWithRegularValues = null as List<Int>?
-        l.listWithNullableValues = listOf(null) as List<Int?>
-        l.nullableListWithNullableValues = listOf(null) as List<Int?>?
-
-        val jj = DomainClassWrapper.newBuilder()
-            .setDomainClass(DomainClass.newBuilder()
-                .setField1("1")
-                .setField2(2)
-                .build())
-            .setIndependentEnum(EnumClass.SECOND)
-            .setNullableDomainClass(null)
-            .setNullableIndependentEnum(null)
-
-        val j = jj.build()
-
-        val b1: DomainClass = j.domainClass
-        val b2: EnumClass = j.independentEnum
-        val b3: DomainClass? = j.nullableDomainClass
-        val b4: EnumClass? = j.nullableIndependentEnum
-
-        val c1: DomainClass? = jj.domainClass
-        val c2: EnumClass? = jj.independentEnum
-        val c3: DomainClass? = jj.nullableDomainClass
-        val c4: EnumClass? = jj.nullableIndependentEnum
-
-        val c1b = jj.domainClassBuilder
-        val c3b = jj.nullableDomainClassBuilder
-
-        j.domainClass = DomainClass.newBuilder().setField2(3).setField1("3").build()
-
-        println(j)
+        listsObject.listWithRegularValues = listOf(1) as List<Int>
+        listsObject.nullableListWithRegularValues = null as List<Int>?
+        listsObject.listWithNullableValues = listOf(null) as List<Int?>
+        listsObject.nullableListWithNullableValues = listOf(null) as List<Int?>?
+        listsObject.listWithRegularListWithRegularValues = listOf(listOf(1)) as List<List<Int>>
+        listsObject.listWithRegularListWithNullableValues = listOf(listOf(null)) as List<List<Int?>>
+        listsObject.listWithNullableListWithRegularValues = listOf(null) as List<List<Int>?>
+        listsObject.listWithNullableListWithNullableValues = listOf(null) as List<List<Int?>?>
+        listsObject.nullableListWithRegularListWithRegularValues = null as List<List<Int>>?
+        listsObject.nullableListWithRegularListWithNullableValues = null as List<List<Int?>>?
+        listsObject.nullableListWithNullableListWithRegularValues = null as List<List<Int>?>?
+        listsObject.nullableListWithNullableListWithNullableValues = null as List<List<Int?>?>?
     }
 
 //    @Test
